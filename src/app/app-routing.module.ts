6e5f06c7aab381db8e849256c9090648835101d2
Sergio Vasconcellos Junior <svasconcellosj@hotmail.com>
2022-01-26 19:17:44 -0300

    Incluído dashboard
    - Incluído dashboard
    - Ajustado tipo categoria direto pela tabela lancamentos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,8 +7,11 @@ import { CategoriaPesquisaComponent } from './categoria/categoria-pesquisa/categ
 import { PlantaPesquisaComponent } from './planta/planta-pesquisa/planta-pesquisa.component';
 import { PlantaCadastroComponent } from './planta/planta-cadastro/planta-cadastro.component';
 import { LancamentoCadastroComponent } from './lancamento/lancamento-cadastro/lancamento-cadastro.component';
+import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
 
   { path: 'lancamentos', component: LancamentoPesquisaComponent },
   { path: 'lancamentos/cadastro', component: LancamentoCadastroComponent },
